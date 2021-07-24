eb3c34abb36c598458679076359052c9ac88868b
Crowdbotics <team@crowdbotics.com>
2021-07-24 18:26:57 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/tiny_frog_29119/MainActivity.java
@@ -0,0 +1,15 @@
+package com.tiny_frog_29119;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "tiny_frog_29119";
+  }
+}
